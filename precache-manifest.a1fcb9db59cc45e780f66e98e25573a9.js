a9a43cc9ac6ca2f43f043ff1717bb60917cf98ff
leungcheryl <leung.cheryl@icloud.com>
2019-07-03 02:39:57 -0400

    Updates

---
+++ precache-manifest.a1fcb9db59cc45e780f66e98e25573a9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7d332c4582c00013715dea149149c0",
+    "url": "/does-the-dog-die/index.html"
+  },
+  {
+    "revision": "d887ff00ad58787cbcd5",
+    "url": "/does-the-dog-die/static/css/main.c3bb9cda.chunk.css"
+  },
+  {
+    "revision": "853ccc33717cf1cbbdff",
+    "url": "/does-the-dog-die/static/js/2.579831fc.chunk.js"
+  },
+  {
+    "revision": "d887ff00ad58787cbcd5",
+    "url": "/does-the-dog-die/static/js/main.ae2465d3.chunk.js"
+  },
+  {
+    "revision": "838132ff8a1752fcf2d8",
+    "url": "/does-the-dog-die/static/js/runtime~main.9f3da749.js"
+  },
+  {
+    "revision": "4c7746e35b620c5f93b7d1841b651fb6",
+    "url": "/does-the-dog-die/static/media/coco-gothic-regular-trial-webfont.4c7746e3.woff"
+  },
+  {
+    "revision": "65a6534eaed13c49082931d3f4049260",
+    "url": "/does-the-dog-die/static/media/coco-gothic-regular-trial-webfont.65a6534e.woff2"
+  },
+  {
+    "revision": "10a0a7219ef6ae33fa687c512bb46b05",
+    "url": "/does-the-dog-die/static/media/dog-1.10a0a721.jpg"
+  },
+  {
+    "revision": "0db5060d4cc5d55f598efc4c185a81a3",
+    "url": "/does-the-dog-die/static/media/img-1.0db5060d.jpg"
+  },
+  {
+    "revision": "0392cb0c6e434076fb413a7256e1d0e2",
+    "url": "/does-the-dog-die/static/media/portraitscript-alts-bold-webfont.0392cb0c.woff"
+  },
+  {
+    "revision": "a0ff4ae0d0332eb7454045d8759236a9",
+    "url": "/does-the-dog-die/static/media/portraitscript-alts-bold-webfont.a0ff4ae0.woff2"
+  },
+  {
+    "revision": "5966984a911ba4f8902ad8ec3b678371",
+    "url": "/does-the-dog-die/static/media/portraitscript-alts-webfont.5966984a.woff2"
+  },
+  {
+    "revision": "5c82e434379e0d457a13defe2f68ab09",
+    "url": "/does-the-dog-die/static/media/portraitscript-alts-webfont.5c82e434.woff"
+  },
+  {
+    "revision": "1b0e9df1932e32655d9b3cc02d92031e",
+    "url": "/does-the-dog-die/static/media/portraitscript-bold-webfont.1b0e9df1.woff"
+  },
+  {
+    "revision": "245a95cf62cd740de0ff30804cf7f9d1",
+    "url": "/does-the-dog-die/static/media/portraitscript-bold-webfont.245a95cf.woff2"
+  },
+  {
+    "revision": "db6ff6d28ead0496ecaf16ce425792b3",
+    "url": "/does-the-dog-die/static/media/portraitscript-bounce-bold-webfont.db6ff6d2.woff"
+  },
+  {
+    "revision": "dfe84be8905e1932da8e0fa1981b7782",
+    "url": "/does-the-dog-die/static/media/portraitscript-bounce-bold-webfont.dfe84be8.woff2"
+  },
+  {
+    "revision": "32b572ee41612f2e4688bc9a0811afac",
+    "url": "/does-the-dog-die/static/media/portraitscript-bounce-webfont.32b572ee.woff2"
+  },
+  {
+    "revision": "e01bdcfaf18d6aacf8fbdf08e1d498bc",
+    "url": "/does-the-dog-die/static/media/portraitscript-bounce-webfont.e01bdcfa.woff"
+  },
+  {
+    "revision": "5cf776d8f3d92e3466919e2c356fb491",
+    "url": "/does-the-dog-die/static/media/portraitscript-regular-webfont.5cf776d8.woff2"
+  },
+  {
+    "revision": "b0c9f306bb5333cb294fbec56a6adebb",
+    "url": "/does-the-dog-die/static/media/portraitscript-regular-webfont.b0c9f306.woff"
+  }
+]);
